7464f6e012f3546cd36a73937865fa533698a290
satyam kumar kesarwani <satyamkesarwani2020@gmail.com>
2026-03-10 10:56:26 +0530

    Remove old src folder

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-import AudiencePortal from "./components/audience/AudiencePortal";
-import JudgePortal from "./components/judge/JudgePortal";
-
-export default function App() {
-  const path=window.location.pathname;
-  if (path.startsWith("/judge")) return <JudgePortal />;
-  return <AudiencePortal />;
-}
